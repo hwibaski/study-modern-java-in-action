85271a92e81495d8726b96005324950155591248
ralph <hwibaski@gmail.com>
2023-07-13 15:43:30 +0900

    initial commit

---
+++ src/main/java/me/hwibaski/modernjava/ModernjavaApplication.java
@@ -0,0 +1,13 @@
+package me.hwibaski.modernjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModernjavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/hwibaski/modernjava/ModernjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hwibaski.modernjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernjavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
