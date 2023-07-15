b37b0f49324684b867a38d3ae6dc5c925fd4287a
ralph <hwibaski@gmail.com>
2023-07-15 18:41:23 +0900

    스트림 소개

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 1. 자바 8, 9, 10, 11 : 무슨 일이 일어나고 있는가?
 2. [동작 파라미터화 코드 전달하기](https://github.com/hwibaski/study-modern-java-in-action/tree/main/src/main/java/me/hwibaski/modernjava/_01_behaviorparameterization)
 3. [람다 표현식](https://github.com/hwibaski/study-modern-java-in-action/tree/main/src/main/java/me/hwibaski/modernjava/_02_lambdaexpression)
+4. [스트림 소개](https://github.com/hwibaski/study-modern-java-in-action/tree/main/src/main/java/me/hwibaski/modernjava/_03_stream_intro)
---
+++ src/main/java/me/hwibaski/modernjava/_03_stream_intro/Main.java
@@ -0,0 +1,26 @@
+package me.hwibaski.modernjava._03_stream_intro;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("1", "2", "3");
+        Stream<String> stream = list.stream();
+        stream.forEach(System.out::println);
+//        stream.forEach(System.out::println);
+
+        List<Dish> menu = Arrays.asList(
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("beef", false, 700, Dish.Type.MEAT),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", false, 530, Dish.Type.OTHER),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("season fruit", true, 120, Dish.Type.OTHER),
+                new Dish("pizza", true, 550, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("salmon", false, 450, Dish.Type.FISH)
+        );
+    }
+}
